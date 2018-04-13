bd1b3088bdec73e9124a3cc074c99459fd53ea0c
Yevgeniy <byevgeniy2010@gmail.com>
2018-04-13 16:02:11 +0300

    add classes and create base ui

---
+++ TestApplication/Controllers/FilterViewController/FilterViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FilterViewController.swift
+//  TestApplication
+//
+//  Created by ba9nist on 13.04.2018.
+//  Copyright © 2018 ba9nist. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TestApplication/Controllers/FollowersViewController/FollowersViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FollowersViewController.swift
+//  TestApplication
+//
+//  Created by ba9nist on 13.04.2018.
+//  Copyright © 2018 ba9nist. All rights reserved.
+//
+
+import UIKit
+
+class FollowersViewController: BaseViewController {
+
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
